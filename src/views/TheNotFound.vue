ad4beb3abf06394a88c897c06ceb7907bb832a18
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-09-10 23:33:20 +0700

    feat: Update notfound

--- src/views/TheNotFound.vue
+++ src/views/TheNotFound.vue
@@ -1,9 +1,26 @@
 <template lang="">
-  <div>Thành phần này đang được phát triển, vui lòng quay lại sau!</div>
+  <div class="notfound">
+    <span class="notfound__404"> 404 not found </span>
+    <span>Thành phần này đang được phát triển, vui lòng quay lại sau!</span>
+  </div>
 </template>
 <script>
 export default {
   name: "TheNotFound",
 };
 </script>
-<style lang=""></style>
+<style scoped>
+.notfound {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 20px;
+}
+.notfound__404 {
+  font-size: 100px;
+  text-transform: uppercase;
+}
+</style>
