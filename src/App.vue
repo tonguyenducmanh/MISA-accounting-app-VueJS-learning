2e63392eb5d3b9884ee7b7766851744f44553011
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-09-10 15:23:49 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import TheDemo from "./views/TheDemo.vue";
+export default {
+  components: {
+    TheDemo,
+  },
+};
+</script>
+
+<template>
+  <the-demo />
+</template>
+
+<style scoped></style>
---
+++ src/views/TheDemo.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <div>Hello xin chào cả nhà mình là chao đây</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
