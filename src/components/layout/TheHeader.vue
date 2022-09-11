67f61ce2fabd91de0ac936c101e672aff43a26c4
Tô Nguyễn Đức Mạnh <ducmanh14032000@gmail.com>
2022-09-11 07:51:41 +0700

    feat: Thêm navbar và header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,5 +1,32 @@
 <template lang="">
-  <div class="header">The header</div>
+  <div class="header">
+    <div class="header__left">
+      <div
+        tabindex="0"
+        class="icon header__menubtn"
+        data-title="Ẩn hiện menu"
+      ></div>
+      <div
+        class="header__name"
+        data-title="Công ty tnhh sản xuất - thương mại - dịch vụ qui phúc"
+      >
+        Công ty tnhh sản xuất - thương mại - dịch vụ qui phúc
+      </div>
+      <div
+        tabindex="0"
+        class="icon header__dropicon"
+        data-title="Công ty tnhh sản xuất - thương mại - dịch vụ qui phúc"
+      ></div>
+    </div>
+    <div class="header__right">
+      <div tabindex="0" class="icon header__noti" data-title="Thông báo"></div>
+      <div class="header__account">
+        <div tabindex="0" class="header__avatar"></div>
+        <div class="header__name">Tô Nguyễn Đức Mạnh</div>
+        <div tabindex="0" class="icon header__drop"></div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,5 +34,6 @@ export default {
 };
 </script>
 <style scoped>
+@import url("../../css/base/icon.css");
 @import url("../../css/layout/header.css");
 </style>
--- src/components/layout/TheNavbar.vue
+++ src/components/layout/TheNavbar.vue
@@ -1,20 +1,58 @@
 <template lang="">
   <div class="navbar">
-    <div class="link"><router-link to="/">Trang chủ</router-link></div>
-    <div class="link"><router-link to="/demo">Trang demo</router-link></div>
-    <div class="link"><router-link to="/khac">Trang khác</router-link></div>
-    <div class="link"><router-link to="/khac">Trang khác</router-link></div>
-    <div class="link"><router-link to="/khac">Trang khác</router-link></div>
-    <div class="link"><router-link to="/khac">Trang khác</router-link></div>
-    <div class="link"><router-link to="/khac">Trang khác</router-link></div>
+    <div class="navbar__top">
+      <div tabindex="0" class="icon navbar__button"></div>
+      <div class="navbar__logo"></div>
+      <div tabindex="0" class="navbar__title" data-title="Về trang chủ">
+        Kế toán
+      </div>
+    </div>
+    <div class="navbar__list">
+      <MRouterLink name="Tổng quan" link="/demo" className="navbar__tq" />
+      <MRouterLink name="Tiền mặt" link="/" className="navbar__tm" />
+      <MRouterLink name="Tiền gửi" link="/tiengui" className="navbar__tg" />
+      <MRouterLink name="Mua hàng" link="/muahang" className="navbar__mh" />
+      <MRouterLink name="Bán hàng" link="/banhang" className="navbar__bh" />
+      <MRouterLink
+        name="Quản lý hóa đơn"
+        link="/hoadon"
+        className="navbar__qlhd"
+      />
+      <MRouterLink name="Kho" link="/kho" className="navbar__k" />
+      <MRouterLink
+        name="Công cụ dụng cụ"
+        link="/congcu"
+        className="navbar__ccdc"
+      />
+      <MRouterLink
+        name="Tài sản cố định"
+        link="/taisancodinh"
+        className="navbar__tscd"
+      />
+      <MRouterLink name="Thuế" link="/thue" className="navbar__thue" />
+      <MRouterLink name="Giá thành" link="/giathanh" className="navbar__gt" />
+      <MRouterLink name="Tổng hợp" link="/tonghop" className="navbar__th" />
+      <MRouterLink name="Ngân sách" link="/ngansach" className="navbar__ns" />
+      <MRouterLink name="Báo cáo" link="/baocao" className="navbar__bc" />
+      <MRouterLink
+        name="Phân tích tài chính"
+        link="/taichinh"
+        className="navbar__pttc"
+      />
+    </div>
   </div>
 </template>
 <script>
+import MRouterLink from "../base/MRouterLink.vue";
 export default {
   name: "TheNavbar",
+  components: {
+    MRouterLink,
+  },
 };
 </script>
-<style scoped>
+<style>
+@import url("../../css/base/icon.css");
 @import url("../../css/layout/navbar.css");
 /* phần dưới để test vue router */
 .link {
